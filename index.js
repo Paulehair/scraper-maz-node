651411c87f53328bdaf083c6a069e75e54f61db1
Paule Herman <p.herman@mazarinedigital.com>
2020-07-28 18:07:22 +0200

    recursive func to check which node

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require('axios')
+const cheerio = require('cheerio')
 
-	process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-    const url = 'https://localhost/contribution/2020/20K/plp/cj//index.html';
+	process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
+    const url = 'https://localhost/contribution/2020/Eyewear/03-us-optical-launch//index.html'
 
     axios(url)
       .then(response => {
-		const html = response.data;
-		const $ = cheerio.load(html);
-		const components = $('.fs-fluidcomponent');
-		const idComponents = []
-		components.each(function () {
-			const id = $(this).attr('id');
-			idComponents.push(id);
-		});
-		console.log(idComponents)
+		const html = response.data
+		const $ = cheerio.load(html)
+	
+		const getChildId = (node, tabSize) => {
+			console.log(tabSize);
+			const tab = '	'.repeat(tabSize);
+			console.log(`#${node.attr('id')}`)
+			const children = node.children('.fs-fluidcomponent')
+			console.log(children.length);
+			children.each(function (i, el) {
+				tabSize += 1
+				getChildId($(el), tabSize)
+			})
+			tabSize = 1
+		}
+
+		const components = $('.fs-fluidcomponent')
+		getChildId(components, 0)
+		// console.log(components.children('.fs-fluidcomponent').first().attr('id'))
+		
+		// components.each(function () {
+		// 	const id = $(this).attr('id');
+		// 	console.log(`#${id}\n`)
+		// });
       })
-	  .catch(console.error);
+	  .catch(console.error)
 
 /**
  * 1/ add mobile version with new axios request using mobile user agent
